3f39bb4f180a39bc8fef022c5c0ec9bb4af42692
András Weisz <andras.weisz@stud.uni-corvinus.hu>
2023-03-07 09:27:03 +0100

    Users list

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -9,13 +9,13 @@ namespace UserMaintenance.Entities
     class User
     {
         public Guid ID { get; set; }
-        public string Firstname { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName
         {
             get
             {
-                return string.Format("{0} {1}", LastName, Firstname);
+                return string.Format("{0} {1}", LastName, FirstName);
             }
         }
     }
